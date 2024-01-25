71bb90c10f5f384f9b3f228f1a98b9e7e5e13745
Jake <jakethacker80@gmail.com>
2024-01-25 22:21:23 +1300

    update eslint

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className="col-md-6 col-12">
           <h1 className="title"> Freedom and Sovereignty in Every Transaction  </h1>
           <p className="py-4">Born too late to explore the seas, born to early to explore the stars, born just in time to witness the rise of the cryptocurrency revolution.</p>
-          <a href="https://www.google.com">
+          <a href="https://docs.google.com/forms/d/1x2nblBXavQ6lek-OtlGGlaEmBVhXyOy8VwGYsgJNZcU/viewform?edit_requested=true">
           <button className="btn-positivus">Join the club</button>
           </a>
         </div>
--- src/container/Team/Team.jsx
+++ src/container/Team/Team.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Headings } from '../../components';
 import { data } from '../../constants';
 import { BsLinkedin } from 'react-icons/bs';
@@ -11,24 +11,26 @@ const Team = () => {
 
       <div className="row">
         {data.Team.map(({ name, position, info, foto, linkedin }, index) => (
-          <div className="col-lg-4 col-md-6 col-12">
-            <div className="card-team" key={index}>
-              <div className="card-team-header"> <img src={foto} alt={name} />
-                <p> <span>{name}</span>
-                  {position}</p>
-                <a href={linkedin} alt={`Linkedin + ${name}`} ><BsLinkedin /></a>
+          <div className="col-lg-4 col-md-6 col-12" key={index}> {/* Added key prop */}
+            <div className="card-team">
+              <div className="card-team-header">
+                <img src={foto} alt={name} />
+                <p>
+                  <span>{name}</span>
+                  {position}
+                </p>
+                <a href={linkedin} alt={`Linkedin + ${name}`}><BsLinkedin /></a>
               </div>
               <p>{info}</p>
             </div>
           </div>
         ))}
-
       </div>
       <div className="d-flex mt-2 justify-content-md-end justify-content-center">
-      <button className="btn-positivus">See all team</button>
+        <button className="btn-positivus">See all team</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
